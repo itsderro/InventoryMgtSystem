69a958265d9817acfbb77111726f5bd5def899e1
Derrick_340 <richardderrick340@gmail.com>
2026-06-23 14:31:44 +0300

    Exceptions
    Added error handling

---
+++ src/main/java/com/tesfayedev/InventoryMgtSystem/exceptions/NameValueRequiredException.java
@@ -0,0 +1,8 @@
+package com.tesfayedev.InventoryMgtSystem.exceptions;
+
+public class NameValueRequiredException extends RuntimeException{
+
+    public NameValueRequiredException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/tesfayedev/InventoryMgtSystem/security/AuthUser.java
@@ -0,0 +1,52 @@
+package com.tesfayedev.InventoryMgtSystem.security;
+
+import com.tesfayedev.InventoryMgtSystem.models.User;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+public class AuthUser implements UserDetails {
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
